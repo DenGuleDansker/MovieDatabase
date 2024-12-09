36029cd868e9606a8b80ede2325669f1bde53fdc
denguledansker <chilinhm@gmail.com>
2024-12-09 16:55:46 +0100

    change for moviepicker

--- CodeTestWexo/CodeTestWexo/obj/Debug/net8.0/CodeTestWexo.AssemblyInfo.cs
+++ CodeTestWexo/CodeTestWexo/obj/Debug/net8.0/CodeTestWexo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeTestWexo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77061664f2c09c5a953e01e343babc11f69b675f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c290d3381225db44a1d7b8e0b19cd8bc3fe0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeTestWexo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeTestWexo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
